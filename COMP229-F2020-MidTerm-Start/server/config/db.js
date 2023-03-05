024ee526ae51efb203df852b8f5f58f4abd86eb9
Magnobar <122378156+Magnobar@users.noreply.github.com>
2023-03-05 16:17:23 -0500

    initial commit

---
+++ COMP229-F2020-MidTerm-Start/server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Elizabeth:<52214461>@cluster0.jopksbd.mongodb.net/books229?retryWrites=true&w=majority"
+};
